fb59d3d530175a04f84cc47f0c2bc6ffbad96b1b
Tobias <tobias@olsson.be>
2018-04-17 07:33:34 +0200

    Add copyright header and some small javadoc

--- api/src/main/java/io/minio/MinioRequestBody.java
+++ api/src/main/java/io/minio/MinioRequestBody.java
@@ -1,3 +1,19 @@
+/*
+ * Minio Java SDK for Amazon S3 Compatible Cloud Storage, (C) 2015 Minio, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.minio;
 
 import java.io.IOException;
@@ -10,6 +26,9 @@ import okhttp3.RequestBody;
 import okio.BufferedSink;
 import okio.Okio;
 
+/**
+ * RequestBody with that wraps a single data object
+ */
 class MinioRequestBody extends RequestBody {
   private final String contentType;
   private final Object data;
