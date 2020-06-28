286d9168c881df596f6864d4c356b259b925e2bb
egetman <getman.eugene@gmail.com>
2020-06-28 19:23:41 +0300

    Add struct for access token & token expiration

---
+++ api/src/main/java/io/minio/messages/ClientGrantsToken.java
@@ -0,0 +1,23 @@
+package io.minio.messages;
+
+import java.util.Objects;
+import javax.annotation.Nonnull;
+
+public class ClientGrantsToken {
+
+    private final String token;
+    private final long expiredAfter;
+
+    public ClientGrantsToken(@Nonnull String token, long expiredAfter) {
+        this.token = Objects.requireNonNull(token);
+        this.expiredAfter = expiredAfter;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public long getExpiredAfter() {
+        return expiredAfter;
+    }
+}
