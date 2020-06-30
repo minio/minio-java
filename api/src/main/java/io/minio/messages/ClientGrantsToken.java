fb86cf0ad0014867a86ee18f678e51bd8eaa3534
egetman <getman.eugene@gmail.com>
2020-06-30 19:54:21 +0300

    Update STS request/response structures

--- api/src/main/java/io/minio/messages/ClientGrantsToken.java
+++ api/src/main/java/io/minio/messages/ClientGrantsToken.java
@@ -2,22 +2,34 @@ package io.minio.messages;
 
 import java.util.Objects;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public class ClientGrantsToken {
 
-  private final String token;
+  private final String jwtAccessToken;
   private final long expiredAfter;
+  private final String policy;
 
-  public ClientGrantsToken(@Nonnull String token, long expiredAfter) {
-    this.token = Objects.requireNonNull(token);
+  @SuppressWarnings("unused")
+  public ClientGrantsToken(@Nonnull String jwtAccessToken, long expiredAfter) {
+    this(jwtAccessToken, expiredAfter, null);
+  }
+
+  public ClientGrantsToken(@Nonnull String jwtAccessToken, long expiredAfter, @Nullable String policy) {
+    this.jwtAccessToken = Objects.requireNonNull(jwtAccessToken);
     this.expiredAfter = expiredAfter;
+    this.policy = policy;
   }
 
   public String token() {
-    return token;
+    return jwtAccessToken;
   }
 
   public long expiredAfter() {
     return expiredAfter;
+  }
+
+  public String policy() {
+    return policy;
   }
 }
--- api/src/main/java/io/minio/messages/Credentials.java
+++ api/src/main/java/io/minio/messages/Credentials.java
@@ -1,48 +1,79 @@
 package io.minio.messages;
 
 import java.time.ZonedDateTime;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
 
 @Root(name = "Credentials")
 public class Credentials {
 
-  @Element(name = "AccessKeyId")
-  private String accessKey;
+    @Element(name = "AccessKeyId")
+    private String accessKey;
 
-  @Element(name = "SecretAccessKey")
-  private String secretKey;
+    @Element(name = "SecretAccessKey")
+    private String secretKey;
 
-  @Element(name = "Expiration")
-  private ResponseDate expiredAt;
+    @Element(name = "Expiration")
+    private ResponseDate expiredAt;
 
-  @Element(name = "SessionToken")
-  private String sessionToken;
+    @Element(name = "SessionToken")
+    private String sessionToken;
 
-  public String accessKey() {
-    return accessKey;
-  }
+    public Credentials() {
+        // default constructor for deserialization stuff
+    }
 
-  public String secretKey() {
-    return secretKey;
-  }
+    public Credentials(@Nonnull String accessKey, @Nonnull String secretKey) {
+        this.accessKey = Objects.requireNonNull(accessKey, "AccessKey must not be null");
+        this.secretKey = Objects.requireNonNull(secretKey, "SecretKey must not be null");
+        if (accessKey.isEmpty() || secretKey.isEmpty()) {
+            throw new IllegalArgumentException("AccessKey and SecretKey must not be empty");
+        }
+    }
 
-  public ZonedDateTime expiredAt() {
-    return expiredAt.zonedDateTime();
-  }
+    public Credentials(@Nonnull String accessKey, @Nonnull String secretKey, @Nonnull ZonedDateTime expiredAt) {
+        this(accessKey, secretKey);
+        this.expiredAt = new ResponseDate(Objects.requireNonNull(expiredAt));
+    }
 
-  public String sessionToken() {
-    return sessionToken;
-  }
+    public Credentials(@Nonnull String accessKey, @Nonnull String secretKey, @Nonnull ZonedDateTime expiredAt,
+                       @Nullable String sessionToken) {
+        this(accessKey, secretKey, expiredAt);
+        this.sessionToken = sessionToken;
+    }
 
-  @Override
-  public String toString() {
-    final StringBuilder sb = new StringBuilder(getClass().getSimpleName() + " [");
-    sb.append("accessKey: ").append(accessKey);
-    sb.append(", secretKey: ").append(secretKey);
-    sb.append(", expiredAt: ").append(expiredAt);
-    sb.append(", sessionToken: ").append(sessionToken);
-    sb.append(']');
-    return sb.toString();
-  }
+    public String accessKey() {
+        return accessKey;
+    }
+
+    public String secretKey() {
+        return secretKey;
+    }
+
+    public ZonedDateTime expiredAt() {
+        return expiredAt.zonedDateTime();
+    }
+
+    public String sessionToken() {
+        return sessionToken;
+    }
+
+    public boolean isAnonymous() {
+        return accessKey == null && secretKey == null;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder(getClass().getSimpleName() + " [");
+        sb.append("accessKey: ").append(accessKey);
+        sb.append(", secretKey: ").append(secretKey);
+        sb.append(", expiredAt: ").append(expiredAt);
+        sb.append(", sessionToken: ").append(sessionToken);
+        sb.append(']');
+        return sb.toString();
+    }
 }
