c53a11b917300b835de5d64df7a4f17bd11c5366
Bala.FA <bala.gluster@gmail.com>
2020-08-14 14:34:08 +0530

    add IAM AWS credential provider

---
+++ examples/MinioClientWithIamAwsProvider.java
@@ -0,0 +1,39 @@
+/*
+ * MinIO Java SDK for Amazon S3 Compatible Cloud Storage, (C) 2020 MinIO, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import io.minio.MinioClient;
+import io.minio.StatObjectArgs;
+import io.minio.StatObjectResponse;
+import io.minio.credentials.IamAwsProvider;
+import io.minio.credentials.Provider;
+
+public class MinioClientWithIamAwsProvider {
+  public static void main(String[] args) throws Exception {
+    Provider provider = new IamAwsProvider(null, null);
+
+    MinioClient minioClient =
+        MinioClient.builder()
+            .endpoint("https://MINIO-HOST:MINIO-PORT")
+            .credentialsProvider(provider)
+            .build();
+
+    // Get information of an object.
+    StatObjectResponse stat =
+        minioClient.statObject(
+            StatObjectArgs.builder().bucket("my-bucketname").object("my-objectname").build());
+    System.out.println(stat);
+  }
+}
