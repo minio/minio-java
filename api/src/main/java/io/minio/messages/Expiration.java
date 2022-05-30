1577b00aacabc1a7fb7886cc4d72f911ecbe8c7a
Bala FA <bala@minio.io>
2022-05-30 22:27:14 +0530

    Enable setting ExpiredObjectDeleteMarker in Expiration. (#1336)
    Fixes #1335
    
    Signed-off-by: Bala.FA <bala@minio.io>

--- api/src/main/java/io/minio/messages/Expiration.java
+++ api/src/main/java/io/minio/messages/Expiration.java
@@ -32,7 +32,18 @@ public class Expiration extends DateDays {
       @Nullable @Element(name = "Days", required = false) Integer days,
       @Nullable @Element(name = "ExpiredObjectDeleteMarker", required = false)
           Boolean expiredObjectDeleteMarker) {
-    super(date, days);
+    if (expiredObjectDeleteMarker != null) {
+      if (date != null || days != null) {
+        throw new IllegalArgumentException(
+            "ExpiredObjectDeleteMarker must not be provided along with Date and Days");
+      }
+    } else if (date != null ^ days != null) {
+      this.date = date;
+      this.days = days;
+    } else {
+      throw new IllegalArgumentException("Only one of date or days must be set");
+    }
+
     this.expiredObjectDeleteMarker = expiredObjectDeleteMarker;
   }
 
