8d6124e1ee79c6fa938b0fe44d4c4411ca31edfc
butterflyzh <butterflyzhu@126.com>
2022-06-09 13:16:55 +0800

    Add License header

--- adminapi/src/main/java/io/minio/admin/QuotaUnit.java
+++ adminapi/src/main/java/io/minio/admin/QuotaUnit.java
@@ -1,3 +1,20 @@
+/*
+ * MinIO Java SDK for Amazon S3 Compatible Cloud Storage,
+ * (C) 2021 MinIO, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.minio.admin;
 
 /** Bucket Quota QuotaUnit. */
@@ -18,7 +35,8 @@ public enum QuotaUnit {
         long totalSize = size * this.unit;
         if (totalSize < 0) {
             throw new IllegalArgumentException("Quota size must be greater than zero.But actual is " + totalSize);
-        } else if (totalSize / this.unit != size) {
+        }
+        if (totalSize / this.unit != size) {
             throw new IllegalArgumentException("Quota size overflow");
         }
         return totalSize;
