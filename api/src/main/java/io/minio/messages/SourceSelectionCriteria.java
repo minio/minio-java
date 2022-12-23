865adf679ae60dcdb434f130dbcbcd06fef98bf6
Bala.FA <bala@minio.io>
2022-12-23 13:25:27 +0530

    Add ReplicationConfiguration/Rule/Destination/SourceSelectionCriteria/ReplicaModifications
    Fixes #1401
    
    Signed-off-by: Bala.FA <bala@minio.io>

--- api/src/main/java/io/minio/messages/SourceSelectionCriteria.java
+++ api/src/main/java/io/minio/messages/SourceSelectionCriteria.java
@@ -23,13 +23,27 @@ import org.simpleframework.xml.Root;
 /** Helper class to denote source selection criteria information for {@link ReplicationRule}. */
 @Root(name = "SourceSelectionCriteria")
 public class SourceSelectionCriteria {
+  @Element(name = "ReplicaModifications", required = false)
+  private ReplicaModifications replicaModifications;
+
   @Element(name = "SseKmsEncryptedObjects", required = false)
   private SseKmsEncryptedObjects sseKmsEncryptedObjects;
 
   public SourceSelectionCriteria(
       @Nullable @Element(name = "SseKmsEncryptedObjects", required = false)
-          SseKmsEncryptedObjects sseKmsEncryptedObjects) {
+          SseKmsEncryptedObjects sseKmsEncryptedObjects,
+      @Nullable @Element(name = "ReplicaModifications", required = false)
+          ReplicaModifications replicaModifications) {
     this.sseKmsEncryptedObjects = sseKmsEncryptedObjects;
+    this.replicaModifications = replicaModifications;
+  }
+
+  public SourceSelectionCriteria(@Nullable SseKmsEncryptedObjects sseKmsEncryptedObjects) {
+    this(sseKmsEncryptedObjects, null);
+  }
+
+  public ReplicaModifications replicaModifications() {
+    return this.replicaModifications;
   }
 
   public SseKmsEncryptedObjects sseKmsEncryptedObjects() {
