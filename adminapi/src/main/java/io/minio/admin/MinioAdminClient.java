ad4ecb751f5f14c7a48261431d0a1bb59544b284
dormanze <37854724+dormanze@users.noreply.github.com>
2023-07-04 15:32:09 +0800

    Update MinioAdminClient.java

--- adminapi/src/main/java/io/minio/admin/MinioAdminClient.java
+++ adminapi/src/main/java/io/minio/admin/MinioAdminClient.java
@@ -606,8 +606,7 @@ public class MinioAdminClient {
    * @throws InvalidKeyException thrown to indicate missing of HMAC SHA-256 library.
    * @throws IOException thrown to indicate I/O error on MinIO REST operation.
    */
-  public String getAdminInfo()
-          throws IOException, NoSuchAlgorithmException, InvalidKeyException {
+  public String getAdminInfo() throws IOException, NoSuchAlgorithmException, InvalidKeyException {
     try (Response response = execute(Method.GET, Command.ADMIN_INFO, null, null)) {
       byte[] jsonData = response.body().bytes();
       return new String(jsonData);
