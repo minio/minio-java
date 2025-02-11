e9726fe63e2027555da233448b9c8a8d74b84602
jongmin-chung <chungjm0711@gmail.com>
2025-02-11 16:19:45 +0900

    fix: add missing fields to listServiceAccount

--- adminapi/src/main/java/io/minio/admin/ListServiceAccountResp.java
+++ adminapi/src/main/java/io/minio/admin/ListServiceAccountResp.java
@@ -17,8 +17,10 @@
 
 package io.minio.admin;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 import java.util.List;
 
 /** list service account response. */
@@ -38,12 +40,64 @@ public class ListServiceAccountResp {
     @JsonProperty("expiration")
     private String expiration;
 
+    @JsonProperty("parentUser")
+    private String parentUser;
+
+    @JsonProperty("accountStatus")
+    private AccountStatus accountStatus;
+
+    @JsonProperty("impliedPolicy")
+    private boolean impliedPolicy;
+
     public String expiration() {
       return expiration;
     }
 
     public String accessKey() {
       return accessKey;
+    }
+
+    public String parentUser() {
+      return parentUser;
+    }
+
+    public AccountStatus accountStatus() {
+      return accountStatus;
+    }
+
+    public boolean impliedPolicy() {
+      return impliedPolicy;
+    }
+
+    public enum AccountStatus {
+      ON("on"),
+      OFF("off");
+
+      private final String value;
+
+      AccountStatus(String value) {
+        this.value = value;
+      }
+
+      @JsonValue
+      public String value() {
+        return value;
+      }
+
+      @JsonCreator
+      public static AccountStatus fromValue(String value) {
+        for (AccountStatus v : AccountStatus.values()) {
+          if (v.value.equals(value)) {
+            return v;
+          }
+        }
+
+        if (value.isEmpty()) {
+          return null;
+        }
+
+        throw new IllegalArgumentException("Unknown account status: " + value);
+      }
     }
   }
 }
